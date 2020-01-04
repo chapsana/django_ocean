446f6baba304ae9826bbf22db799b9c636cc2dfe
Alpha Olomi <alphaolomi@gmail.com>
2020-01-04 11:33:27 +0300

    Update views.py

--- ocean_project/droplets/views.py
+++ ocean_project/droplets/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class GetDroplets(TemplateView):
+    template_name = 'droplets.html'
+    def get_context_data(self, *args, **kwargs):
+        pass
